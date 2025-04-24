588433f084bd05152adcc633192c2c96bfc74b6f
Harry <sanchezmormiguel815@gmail.com>
2025-04-23 23:24:55 -0500

    f

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import './App.css'
 import Jilian from './Inter-Usu/Jillian-App/Jilian'
 import Jilian2 from './Inter-Usu/Jillian-App/Jilian-2'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HeaderMi from './home/Componentes-Miguel/HeaderMi/MenuMi';
 import Miguel from './home/Miguel/Miguel';
 import Carlos from './Inter-pro/Carlos apps/carlos';
 import Carlos2 from './Inter-pro/Carlos apps/Carlos2';
-import Home from './registro/Pages/Home';
 import Registro from "./registro/Pages/Registro";
-import Login from './Login/Login';
 
 function App() {
 
