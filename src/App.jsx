906dd70c4fc32f16cfe9aaefc9a326dccca81681
Pet <Jossimartinez0912@gmail.com>
2025-06-04 13:48:59 -0500

    45

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import Jilian from './Inter-Usu/Jillian-App/Jilian'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Miguel from './home/Miguel/Miguel';
+import Login from './Iniciarsesion/Componentesjossi/Login';
 import Carlos from './Inter-pro/Carlos apps/carlos';
 import Carlos2 from './Inter-pro/Carlos apps/Carlos2';
 import Registro from "./registro/Pages/Registro";
 import Carlos3 from './Inter-pro/Carlos apps/Carlos3';
+
 
 function App() {
 
@@ -18,7 +20,7 @@ function App() {
       <Route path='/inter-Usu' element={<Carlos></Carlos>}></Route>
       <Route path='/Inter-Usu2' element={<Carlos2></Carlos2>}></Route>
       <Route path='/Inter-Usu3' element={<Carlos3></Carlos3>}></Route>
-      <Route path='/Inter-Pro' element={<Jilian></Jilian>}></Route>
+      <Route path='/Inter-Pro' element={<Login></Login>}></Route>
     </Routes>
          
     </>
