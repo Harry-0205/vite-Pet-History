75e08de16ceebc204af35881cea3e0db347e3433
hola <hgola@example.com>
2025-07-03 11:33:36 -0500

    integracion

---
+++ src/home/Miguel/Miguel.jsx
@@ -0,0 +1,19 @@
+import HeaderMi from "../Componentes-Miguel/HeaderMi/HeaderMi"
+import MainMi from "../Componentes-Miguel/MainMi/MainMi"
+import FooterMi from "../Componentes-Miguel/FooterMi/FooterMi"
+
+
+function Miguel(){
+    return(
+        <>
+         <div className="acomodacion">
+        <HeaderMi></HeaderMi>
+        <MainMi></MainMi>
+        <FooterMi></FooterMi>
+        </div>
+        </>
+
+      
+    )
+}
+export default Miguel
