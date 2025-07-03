b1519b05c6149d94e6dc1b695755d561eb49413e
hola <hgola@example.com>
2025-07-03 11:48:17 -0500

    minicambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { Route, Routes } from 'react-router-dom'
 import Jilian from './Inter-Usu/Jillian-App/Jilian'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Miguel from './home/Miguel/Miguel'
-import Login from './Iniciarsesion/Componentesjossi/Login';
 import Carlos from './Inter-pro/Carlos apps/carlos';
 import Carlos2 from './Inter-pro/Carlos apps/Carlos2';
 import Registro from "./registro/Pages/Registro";
 import Carlos3 from './Inter-pro/Carlos apps/Carlos3';
+import Login from './Iniciarsesion/Componentesjossi/Login';
 
 
 function App() {
 
   return (
     <>
-      <Miguel></Miguel>
+   
     </>
   )
 }
