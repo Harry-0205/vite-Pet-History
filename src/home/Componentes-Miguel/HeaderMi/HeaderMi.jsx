52b2f03e7e9193f98357a9e44aefd8844bb1fddf
Harry <sanchezmormiguel815@gmail.com>
2025-09-10 22:02:03 -0500

    diseño-1

--- src/home/Componentes-Miguel/HeaderMi/HeaderMi.jsx
+++ src/home/Componentes-Miguel/HeaderMi/HeaderMi.jsx
@@ -5,7 +5,7 @@ function HeaderMi (){
     return(
         <header className='heMi'>
             <div><img src={logo} alt="" className='logo'/></div>
-            <div>
+            <div className='heMi'>
                 <div><h6>Inicio</h6></div>
                 <div><h6>Servicios</h6></div>
                 <div><h6>¿Quienes somos?</h6></div>
