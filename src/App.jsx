5320364390db99c2f0751cd402772b9a1b6f86c2
Bando <araqueb5@gmail.com>
2025-09-29 03:29:54 -0500

    Visuales bando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Miguel from './home/Miguel/Miguel'
+import Register from './Registro/Register'
+import AdminPanel from './admin/AdminPanel'
 import '../src/App.css'
 
 
@@ -8,6 +10,8 @@ function App() {
 
   return (
         <Routes>
+          <Route path='/registro' element={<Register />} />
+          <Route path='/admin' element={<AdminPanel />} />
           <Route path='/*' element={<Miguel/>} />
         </Routes>
   )
